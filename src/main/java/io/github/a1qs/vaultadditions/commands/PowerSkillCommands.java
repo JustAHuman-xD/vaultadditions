33086ea1f644f11bce37e3f7139d7531b3819911
a1qs <ankiller2007.09.20@gmail.com>
2024-10-26 00:00:31 +0200

    Register & add new PowerSkillCommands to reset & add points

---
+++ src/main/java/io/github/a1qs/vaultadditions/commands/PowerSkillCommands.java
@@ -0,0 +1,69 @@
+package io.github.a1qs.vaultadditions.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.a1qs.vaultadditions.test.PlayerSpecialExpertiseData;
+import iskallia.vault.util.calc.*;
+import iskallia.vault.world.data.PlayerVaultStatsData;
+import net.minecraft.Util;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+public class PowerSkillCommands {
+    public PowerSkillCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("vaultadditions")
+                .requires(sender -> sender.hasPermission(this.getRequiredPermissionLevel()))
+                .then(Commands.literal("power")
+                        .then(Commands.literal("resetPowers")
+                                .then(Commands.argument("player", EntityArgument.player())
+                                        .executes(this::resetPowers)
+                                )
+                        )
+                        .then(Commands.literal("addPoints")
+                                .then(Commands.argument("player", EntityArgument.player())
+                                        .then(Commands.argument("amount", IntegerArgumentType.integer())
+                                                .executes(this::addPoints)
+                                        )
+
+                                )
+                        )
+                )
+
+        );
+    }
+
+
+
+    private int resetPowers(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        ServerPlayer player = EntityArgument.getPlayer(context, "player");
+        PlayerSpecialExpertiseData data = PlayerSpecialExpertiseData.get(ServerLifecycleHooks.getCurrentServer());
+        data.resetSpecialExpertiseTree(player);
+        context.getSource().sendSuccess(new TextComponent("Reset " + player.getName().getString() + " Power Skills"), true);
+
+        return 0;
+    }
+
+    private int addPoints(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        int amount = IntegerArgumentType.getInteger(context, "amount");
+        ServerPlayer player = EntityArgument.getPlayer(context, "player");
+        PlayerVaultStatsData data = PlayerVaultStatsData.get(player.getLevel());
+        data.addArchetypePoints(player, amount);
+        context.getSource().sendSuccess(new TextComponent("Added " + amount + " of Archetype points to " + player.getName().getString()), true);
+
+        return 0;
+    }
+
+
+
+
+    public int getRequiredPermissionLevel() {
+        return 2;
+    }
+
+}
--- src/main/java/io/github/a1qs/vaultadditions/events/CommandEvents.java
+++ src/main/java/io/github/a1qs/vaultadditions/events/CommandEvents.java
@@ -2,6 +2,7 @@ package io.github.a1qs.vaultadditions.events;
 
 
 import io.github.a1qs.vaultadditions.VaultAdditions;
+import io.github.a1qs.vaultadditions.commands.PowerSkillCommands;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,6 +13,7 @@ public class CommandEvents {
     @SubscribeEvent
     public static void onCommandsRegister(RegisterCommandsEvent event) {
         new io.github.a1qs.vaultadditions.commands.ConfigCommand(event.getDispatcher());
+        new PowerSkillCommands(event.getDispatcher());
 
         ConfigCommand.register(event.getDispatcher());
     }
