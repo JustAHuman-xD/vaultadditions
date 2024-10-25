6074b444bfa0532729301be49cb612c8144306f2
a1qs <ankiller2007.09.20@gmail.com>
2024-10-25 23:56:25 +0200

    New BorderGemstone functionality, on use grants an Archetype point, shift clicking consumes full stack.

--- src/main/java/io/github/a1qs/vaultadditions/item/BorderGemstone.java
+++ src/main/java/io/github/a1qs/vaultadditions/item/BorderGemstone.java
@@ -1,15 +1,25 @@
 package io.github.a1qs.vaultadditions.item;
 
 import io.github.a1qs.vaultadditions.config.CommonConfigs;
+import iskallia.vault.world.data.PlayerVaultStatsData;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class BorderGemstone extends Item {
@@ -22,4 +32,28 @@ public class BorderGemstone extends Item {
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
         pTooltipComponents.add(new TextComponent("Increases the World Border by ").append(CommonConfigs.BORDER_GEMSTONE_INCREASE.get().toString()).withStyle(ChatFormatting.YELLOW).append(" Blocks!"));
     }
+
+    @Nonnull
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, @Nonnull InteractionHand hand) {
+        ItemStack heldItemStack = player.getItemInHand(hand);
+        world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_LEVELUP, SoundSource.PLAYERS, 0.5F, 0.4F / (world.random.nextFloat() * 0.4F + 0.8F));
+        if (!world.isClientSide) {
+            if (!player.getAbilities().instabuild) {
+                PlayerVaultStatsData statsData = PlayerVaultStatsData.get((ServerLevel) world);
+                if(player.isCrouching()) {
+                    int itemStackCount = heldItemStack.getCount();
+                    heldItemStack.shrink(itemStackCount);
+                    statsData.addArchetypePoints((ServerPlayer) player, itemStackCount);
+                    return InteractionResultHolder.success(heldItemStack);
+                }
+                statsData.addArchetypePoints((ServerPlayer) player, 1);
+                heldItemStack.shrink(1);
+            }
+        }
+        player.awardStat(Stats.ITEM_USED.get(this));
+
+        return InteractionResultHolder.sidedSuccess(heldItemStack, world.isClientSide());
+    }
+
+
 }
