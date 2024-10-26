8babe49ac11d3ba5ef37463746dbbe1329db725a
a1qs <ankiller2007.09.20@gmail.com>
2024-10-26 15:54:28 +0200

    Refactor PowerConfigs to MiscUtil

--- src/main/java/io/github/a1qs/vaultadditions/util/MiscUtil.java
+++ src/main/java/io/github/a1qs/vaultadditions/util/MiscUtil.java
@@ -1,7 +1,9 @@
-package io.github.a1qs.vaultadditions.vault.powers;
+package io.github.a1qs.vaultadditions.util;
 
 
 
+import io.github.a1qs.vaultadditions.vault.powers.SpecialExpertisesConfig;
+import io.github.a1qs.vaultadditions.vault.powers.SpecialExpertisesGUIConfig;
 import iskallia.vault.skill.PlayerVaultStats;
 import iskallia.vault.skill.base.SkillContext;
 import iskallia.vault.skill.source.SkillSource;
@@ -9,9 +11,12 @@ import iskallia.vault.world.data.PlayerVaultStatsData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 
-public class PowerConfigs {
+import java.util.UUID;
+
+public class MiscUtil {
     public static SpecialExpertisesGUIConfig SPECIAL_EXPERTISES_GUI;
     public static SpecialExpertisesConfig SPECIAL_EXPERTISES;
+    public static UUID sizeScaleModifierUUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
 
     public static SkillContext ofSpecialExpertise(ServerPlayer player) {
         PlayerVaultStats stats = PlayerVaultStatsData.get((ServerLevel)player.level).getVaultStats(player);
