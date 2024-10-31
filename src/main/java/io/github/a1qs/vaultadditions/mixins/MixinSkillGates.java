a2c17a3fb84a5976be8d64422d8d64e7be9e1551
a1qs <ankiller2007.09.20@gmail.com>
2024-10-31 18:54:40 +0100

    add Power skills to "getAllSkillIds" method

---
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinSkillGates.java
@@ -0,0 +1,27 @@
+package io.github.a1qs.vaultadditions.mixins;
+
+import io.github.a1qs.vaultadditions.util.MiscUtil;
+import iskallia.vault.skill.SkillGates;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
+
+@Mixin(value = SkillGates.class, remap = false)
+public class MixinSkillGates {
+
+    @ModifyVariable(
+            method = "getAllSkillIds",
+            at = @At(value = "STORE", ordinal = 0),
+            ordinal = 0
+    )
+    private List<String> addPowerSkills(List<String> allSkillIds) {
+        MiscUtil.POWERS.getAll().skills.forEach((s) -> {
+            allSkillIds.add(s.getId());
+        });
+        System.out.println(allSkillIds);
+
+        return allSkillIds;
+    }
+}
