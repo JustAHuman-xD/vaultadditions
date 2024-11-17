964e4f29c1c05c56415d1e3691b6fd9c7cfdbab4
a1qs <ankiller2007.09.20@gmail.com>
2024-11-17 17:14:21 +0100

    register new EventCommands

--- src/main/java/io/github/a1qs/vaultadditions/events/CommandEvents.java
+++ src/main/java/io/github/a1qs/vaultadditions/events/CommandEvents.java
@@ -3,6 +3,7 @@ package io.github.a1qs.vaultadditions.events;
 
 import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.commands.CrystalCommands;
+import io.github.a1qs.vaultadditions.commands.EventCommands;
 import io.github.a1qs.vaultadditions.commands.PowerSkillCommands;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,6 +17,7 @@ public class CommandEvents {
         new io.github.a1qs.vaultadditions.commands.ConfigCommand(event.getDispatcher());
         new PowerSkillCommands(event.getDispatcher());
         new CrystalCommands(event.getDispatcher());
+        new EventCommands(event.getDispatcher());
 
         ConfigCommand.register(event.getDispatcher());
     }
