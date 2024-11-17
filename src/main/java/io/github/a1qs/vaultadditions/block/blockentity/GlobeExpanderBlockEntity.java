651236e4a125e1afc4c1621c8497d69705cc5f1d
a1qs <ankiller2007.09.20@gmail.com>
2024-11-17 20:21:24 +0100

    change color formatting for consistency

--- src/main/java/io/github/a1qs/vaultadditions/block/blockentity/GlobeExpanderBlockEntity.java
+++ src/main/java/io/github/a1qs/vaultadditions/block/blockentity/GlobeExpanderBlockEntity.java
@@ -70,9 +70,9 @@ public class GlobeExpanderBlockEntity extends BlockEntity {
                         data.getActiveEvent().addCrystalsSubmitted((int) blocksExpanded / ServerConfigs.POWER_CRYSTAL_INCREASE.get());
                         if(data.getActiveEvent().isModifierActive()) {
                             MutableComponent eventComponent2 = new TextComponent("[EVENT] ").withStyle(ChatFormatting.YELLOW, ChatFormatting.BOLD)
-                                    .append(new TextComponent("The Event: ").setStyle(Style.EMPTY.withColor(ChatFormatting.LIGHT_PURPLE).withBold(false)))
-                                    .append(new TextComponent(data.getActiveEvent().getEventMessage()).setStyle(Style.EMPTY.withColor(ChatFormatting.YELLOW).withBold(false)))
-                                    .append(new TextComponent(" is now active").setStyle(Style.EMPTY.withColor(ChatFormatting.LIGHT_PURPLE).withBold(false)));
+                                    .append(new TextComponent("The Event: ").setStyle(Style.EMPTY.withColor(ChatFormatting.YELLOW).withBold(false)))
+                                    .append(new TextComponent(data.getActiveEvent().getEventMessage()).setStyle(Style.EMPTY.withColor(ChatFormatting.LIGHT_PURPLE).withBold(false)))
+                                    .append(new TextComponent(" is now active").setStyle(Style.EMPTY.withColor(ChatFormatting.YELLOW).withBold(false)));
 
 
                             srv.getPlayerList().broadcastMessage(eventComponent2, ChatType.SYSTEM, Util.NIL_UUID);
