2007fe3f304140b4f3f1273ff4ed3c6fedb4b977
a1qs <ankiller2007.09.20@gmail.com>
2024-11-27 21:42:20 +0100

    add LootStatueBlock with model, translation & registration

---
+++ src/main/java/io/github/a1qs/vaultadditions/block/LootStatueBlock.java
@@ -0,0 +1,218 @@
+package io.github.a1qs.vaultadditions.block;
+
+
+import io.github.a1qs.vaultadditions.block.blockentity.LootStatueBlockEntity;
+import io.github.a1qs.vaultadditions.container.LootStatueContainer;
+import io.github.a1qs.vaultadditions.container.RenameContainer;
+import io.github.a1qs.vaultadditions.init.ModBlockEntities;
+import io.github.a1qs.vaultadditions.util.MiscUtil;
+import iskallia.vault.block.entity.LootStatueTileEntity;
+import iskallia.vault.init.ModConfigs;
+import iskallia.vault.util.RenameType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+public class LootStatueBlock extends BaseEntityBlock {
+    private static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    private static final VoxelShape SHAPE;
+
+    public LootStatueBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.SOUTH));
+
+    }
+
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void setPlacedBy(Level pLevel, BlockPos pos, BlockState pState, LivingEntity pPlacer, ItemStack stack) {
+        if (pPlacer instanceof ServerPlayer player) {
+            BlockEntity var9 = pLevel.getBlockEntity(pos);
+            if (var9 instanceof LootStatueBlockEntity be) {
+                if (!stack.hasTag() || !stack.getTag().contains("BlockEntityTag")) {
+                    final CompoundTag data = new CompoundTag();
+                    ListTag itemList = new ListTag();
+                    List<ItemStack> options = ModConfigs.STATUE_LOOT.getOptions();
+
+                    for (ItemStack option : options) {
+                        itemList.add(option.serializeNBT());
+                    }
+
+                    data.put("Items", itemList);
+                    data.put("Position", NbtUtils.writeBlockPos(pos));
+                    be.getSkin().updateSkin("a1qs");
+                    NetworkHooks.openGui(player, new MenuProvider() {
+                        public Component getDisplayName() {
+                            return new TextComponent("Loot Statue Options");
+                        }
+
+                        public AbstractContainerMenu createMenu(int windowId, Inventory playerInventory, Player playerEntity) {
+                            return new LootStatueContainer(windowId, data);
+                        }
+                    }, (buffer) -> {
+                        buffer.writeNbt(data);
+                    });
+                }
+            }
+        }
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
+        world.getBlockEntity(pos);
+        BlockEntity var9 = world.getBlockEntity(pos);
+        if (var9 instanceof LootStatueBlockEntity statue) {
+            ItemStack heldItem = player.getMainHandItem();
+            if (player.isSecondaryUseActive()) {
+                if (heldItem.isEmpty()) {
+                    if (player instanceof ServerPlayer serverPlayer) {
+                        final CompoundTag nbt = new CompoundTag();
+                        nbt.putInt("RenameType", RenameType.PLAYER_STATUE.ordinal());
+                        CompoundTag data = statue.saveWithoutMetadata();
+                        data.put("Pos", NbtUtils.writeBlockPos(pos));
+                        nbt.put("Data", data);
+                        NetworkHooks.openGui(serverPlayer, new MenuProvider() {
+                            public Component getDisplayName() {
+                                return new TextComponent("Rename Player Statue");
+                            }
+
+                            public @NotNull AbstractContainerMenu createMenu(int windowId, Inventory playerInventory, Player playerEntity) {
+                                return new RenameContainer(windowId, nbt);
+                            }
+                        }, (buffer) -> {
+                            buffer.writeNbt(nbt);
+                        });
+                    }
+
+                    return InteractionResult.sidedSuccess(world.isClientSide);
+                }
+            }
+        }
+
+        return super.use(state, world, pos, player, handIn, hit);
+    }
+
+
+
+
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public void playerWillDestroy(Level world, BlockPos pos, BlockState state, Player player) {
+        if (!world.isClientSide) {
+            BlockEntity tileEntity = world.getBlockEntity(pos);
+            ItemStack itemStack = new ItemStack(this);
+            if (tileEntity instanceof LootStatueBlockEntity tile) {
+                CompoundTag stackNBT = new CompoundTag();
+                stackNBT.put("BlockEntityTag", tile.saveWithoutMetadata());
+                itemStack.setTag(stackNBT);
+            }
+
+            ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5, itemStack);
+            itemEntity.setDefaultPickUpDelay();
+            world.addFreshEntity(itemEntity);
+        }
+
+        super.playerWillDestroy(world, pos, state, player);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter world, BlockPos pos, Player player) {
+        ItemStack itemstack = super.getCloneItemStack(state, target, world, pos, player);
+        BlockEntity var8 = world.getBlockEntity(pos);
+        if (var8 instanceof LootStatueBlockEntity tile) {
+            CompoundTag stackNBT = new CompoundTag();
+            stackNBT.put("BlockEntityTag", tile.saveWithoutMetadata());
+            if (!stackNBT.isEmpty()) {
+                itemstack.setTag(stackNBT);
+            }
+        }
+
+        return itemstack;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        return List.of();
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return ModBlockEntities.LOOT_STATUE_BLOCK_ENTITY.get().create(pPos, pState);
+    }
+
+    @Nullable
+    @Override
+    @ParametersAreNonnullByDefault
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        System.out.println(type);
+        System.out.println(ModBlockEntities.LOOT_STATUE_BLOCK_ENTITY.get());
+        return level.isClientSide ? null : createTickerHelper(type, ModBlockEntities.LOOT_STATUE_BLOCK_ENTITY.get(), LootStatueBlockEntity::tick);
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    static {
+        VoxelShape[] shape = {
+                Shapes.box(0.0625, 0, 0.0625, 0.9375, 0.0625, 0.9375),
+                Shapes.box(0.0625, 0.0625, 0.0625, 0.9375, 0.1875, 0.9375)
+        };
+
+        SHAPE = MiscUtil.mergeVoxelShapes(shape);
+    }
+}
--- src/main/java/io/github/a1qs/vaultadditions/init/ModBlocks.java
+++ src/main/java/io/github/a1qs/vaultadditions/init/ModBlocks.java
@@ -3,6 +3,7 @@ package io.github.a1qs.vaultadditions.init;
 import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.block.EventBlock;
 import io.github.a1qs.vaultadditions.block.GlobeExpanderBlock;
+import io.github.a1qs.vaultadditions.block.LootStatueBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -21,15 +22,24 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> GLOBE_EXPANDER = registerBlock("globe_expander",
             () -> new GlobeExpanderBlock(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(-1).noOcclusion()), CreativeModeTab.TAB_TOOLS);
+                    .strength(-1).noOcclusion()), CreativeModeTab.TAB_TOOLS,
+            true);
 
     public static final RegistryObject<Block> EVENT_BLOCK = registerBlock("event_block",
             () -> new EventBlock(BlockBehaviour.Properties.of(Material.BARRIER)
-                    .strength(-1, 3.6E8F).noOcclusion().noCollission().noDrops()), CreativeModeTab.TAB_MISC);
+                    .strength(-1, 3.6E8F).noOcclusion().noCollission().noDrops()), CreativeModeTab.TAB_MISC,
+            true);
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+    public static final RegistryObject<Block> LOOT_STATUE = registerBlock("loot_statue_block",
+            () -> new LootStatueBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(20, 3.6E8F).noOcclusion()),
+            CreativeModeTab.TAB_MISC,
+            false);
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab, boolean createBlockItem) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn, tab);
+        if(createBlockItem) registerBlockItem(name, toReturn, tab);
         return toReturn;
     }
 
