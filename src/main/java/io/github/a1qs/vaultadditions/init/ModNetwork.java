c8f809f75402accf83b1327f29c95755d70bfe89
a1qs <ankiller2007.09.20@gmail.com>
2024-11-27 21:43:39 +0100

    Register new Network messages for the Loot Statue

--- src/main/java/io/github/a1qs/vaultadditions/init/ModNetwork.java
+++ src/main/java/io/github/a1qs/vaultadditions/init/ModNetwork.java
@@ -1,17 +1,14 @@
 package io.github.a1qs.vaultadditions.init;
 
 import io.github.a1qs.vaultadditions.VaultAdditions;
-import io.github.a1qs.vaultadditions.network.KnownPowerMessage;
-import io.github.a1qs.vaultadditions.network.PowerLevelMessage;
-import io.github.a1qs.vaultadditions.network.PowerPointMessage;
-import io.github.a1qs.vaultadditions.network.ServerboundOpenPowersMessage;
+import io.github.a1qs.vaultadditions.network.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class ModNetwork {
     private static final String VERSION = "1.0.0";
-    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(VaultAdditions.MOD_ID, "network"), () -> VERSION, VERSION::equals, VERSION::equals);;
+    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(VaultAdditions.MOD_ID, "network"), () -> VERSION, VERSION::equals, VERSION::equals);
     private static int ID = 0;
 
     public static void initialize() {
@@ -19,6 +16,8 @@ public class ModNetwork {
         CHANNEL.registerMessage(nextId(), KnownPowerMessage.class, KnownPowerMessage::encode, KnownPowerMessage::decode, KnownPowerMessage::handle);
         CHANNEL.registerMessage(nextId(), PowerLevelMessage.class, PowerLevelMessage::encode, PowerLevelMessage::decode, PowerLevelMessage::handle);
         CHANNEL.registerMessage(nextId(), PowerPointMessage.class, PowerPointMessage::encode, PowerPointMessage::decode, PowerPointMessage::handle);
+        CHANNEL.registerMessage(nextId(), StatueSelectItemMessage.class, StatueSelectItemMessage::encode, StatueSelectItemMessage::decode, StatueSelectItemMessage::handle);
+        CHANNEL.registerMessage(nextId(), RenameStatueMessage.class, RenameStatueMessage::encode, RenameStatueMessage::decode, RenameStatueMessage::handle);
     }
 
     public static int nextId() {
