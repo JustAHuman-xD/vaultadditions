ac3ca79436269bf37141bb7cf33bd051cbcbdb0e
a1qs <ankiller2007.09.20@gmail.com>
2024-11-30 21:44:27 +0100

    update eventConfig to include some basic example events

--- src/main/java/io/github/a1qs/vaultadditions/config/vault/EventConfig.java
+++ src/main/java/io/github/a1qs/vaultadditions/config/vault/EventConfig.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.annotations.Expose;
+import io.github.a1qs.vaultadditions.events.Event;
 import io.github.a1qs.vaultadditions.events.EventEntry;
 import iskallia.vault.config.Config;
 import iskallia.vault.util.data.WeightedList;
@@ -25,35 +26,46 @@ public class EventConfig extends Config {
     protected void reset() {
         this.eventList.clear();
 
-        // Create JSON text components
-        JsonElement complexMessage = createComplexTextComponent();
-
         // Add entries to the event list
         eventList.add(new EventEntry(
-                new ResourceLocation("vaultadditions:test1"),
-                complexMessage,
+                Event.BORDER_EXPANSION_ENABLED,
+                createComplexStartMessage(),
+                createComplexEndMessage(),
+                createComplexLoginMessage(),
+                createComplexDisplayMessage(),
                 200L,
                 false), 2);
 
         eventList.add(new EventEntry(
-                new ResourceLocation("vaultadditions:test2"),
-                complexMessage,
+                Event.BORDER_EXPANSION_ENABLED,
+                createComplexStartMessage(),
+                createComplexEndMessage(),
+                createComplexLoginMessage(),
+                createComplexDisplayMessage(),
                 200L,
-                false), 54);
+                true), 2);
 
         eventList.add(new EventEntry(
-                new ResourceLocation("vaultadditions:test3"),
-                complexMessage,
-                200L,
-                true), 54534);
-
-        eventList.add(new EventEntry(
-                        new ResourceLocation("vaultadditions:test4"),
-                        complexMessage,
+                        Event.ADD_PORTAL_MODIFIERS,
+                        createComplexStartMessage(),
+                        createComplexEndMessage(),
+                        createComplexLoginMessage(),
+                        createComplexDisplayMessage(),
                         200L,
                         true,
-                        List.of(new ResourceLocation("the_vault:dev1"), new ResourceLocation("the_vault:dev2"))),
-                3289);
+                        List.of(new ResourceLocation("the_vault:energizing"), new ResourceLocation("the_vault:energizing"), new ResourceLocation("the_vault:energizing"), new ResourceLocation("the_vault:energizing"))),
+                3);
+
+        eventList.add(new EventEntry(
+                        Event.ADD_PORTAL_MODIFIERS,
+                        createComplexStartMessage(),
+                        createComplexEndMessage(),
+                        createComplexLoginMessage(),
+                        createComplexDisplayMessage(),
+                        200L,
+                        true,
+                        List.of(new ResourceLocation("the_vault:energizing"), new ResourceLocation("the_vault:item_quantity"))),
+                3);
     }
 
     public WeightedList<EventEntry> getWeightedList() {
@@ -63,7 +75,7 @@ public class EventConfig extends Config {
 
 
 
-    private JsonElement createComplexTextComponent() {
+    private JsonElement createComplexStartMessage() {
         JsonArray jsonArray = new JsonArray();
 
         // Adding styled text components
@@ -72,23 +84,92 @@ public class EventConfig extends Config {
         part1.addProperty("color", "yellow");
 
         JsonObject part2 = new JsonObject();
-        part2.addProperty("text", "test message");
+        part2.addProperty("text", "start message");
         part2.addProperty("color", "green");
         part2.addProperty("bold", true);
 
         JsonObject part3 = new JsonObject();
-        part3.addProperty("text", " with dynamic content.");
+        part3.addProperty("text", " with dynamic content. (Total Duration: {totalEventDurationTicks} ticks)");
         part3.addProperty("color", "blue");
 
-        JsonObject part4 = new JsonObject();
-        part4.addProperty("text", "\nThis is a test for the placeholders: EVENTDURATION: {eventDuration}");
-        part4.addProperty("color", "red");
 
         // Add parts to the array
         jsonArray.add(part1);
         jsonArray.add(part2);
         jsonArray.add(part3);
-        jsonArray.add(part4);
+
+        return jsonArray;
+    }
+
+    private JsonElement createComplexEndMessage() {
+        JsonArray jsonArray = new JsonArray();
+
+        // Adding styled text components
+        JsonObject part1 = new JsonObject();
+        part1.addProperty("text", "This is an ");
+        part1.addProperty("color", "yellow");
+        part1.addProperty("bold", true);
+
+        JsonObject part2 = new JsonObject();
+        part2.addProperty("text", "end message");
+        part2.addProperty("color", "dark_green");
+
+        JsonObject part3 = new JsonObject();
+        part3.addProperty("text", " with dynamic content. (isCrystalSubmission? {crystalSubmission})");
+        part3.addProperty("color", "gold");
+
+
+        // Add parts to the array
+        jsonArray.add(part1);
+        jsonArray.add(part2);
+        jsonArray.add(part3);
+
+        return jsonArray;
+    }
+
+    private JsonElement createComplexLoginMessage() {
+        JsonArray jsonArray = new JsonArray();
+
+        // Adding styled text components
+        JsonObject part1 = new JsonObject();
+        part1.addProperty("text", "This is a ");
+        part1.addProperty("color", "green");
+
+        JsonObject part2 = new JsonObject();
+        part2.addProperty("text", "login message");
+        part2.addProperty("color", "blue");
+        part2.addProperty("bold", true);
+
+        JsonObject part3 = new JsonObject();
+        part3.addProperty("text", " with dynamic content. (Time Remaining: {eventDurationDays}d, {eventDurationHours}h, {eventDurationMinutes}m, {eventDurationSeconds}s )");
+        part3.addProperty("color", "red");
+
+
+        // Add parts to the array
+        jsonArray.add(part1);
+        jsonArray.add(part2);
+        jsonArray.add(part3);
+
+        return jsonArray;
+    }
+
+    private JsonElement createComplexDisplayMessage() {
+        JsonArray jsonArray = new JsonArray();
+
+        // Adding styled text components
+        JsonObject part1 = new JsonObject();
+        part1.addProperty("text", "Message");
+        part1.addProperty("color", "light_purple");
+
+
+        JsonObject part2 = new JsonObject();
+        part2.addProperty("text", " with dynamic content. (MinCrystalsSubmitted: {minCrystalsSubmitted})");
+        part2.addProperty("color", "red");
+
+
+        // Add parts to the array
+        jsonArray.add(part1);
+        jsonArray.add(part2);
 
         return jsonArray;
     }
