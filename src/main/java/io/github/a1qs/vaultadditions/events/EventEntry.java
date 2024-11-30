f29e162e87c00aebe36f195ac0ad28aadb31f0c9
a1qs <ankiller2007.09.20@gmail.com>
2024-11-30 21:39:11 +0100

    Update EventEntry class for new EventMessages & getter/setter methods

--- src/main/java/io/github/a1qs/vaultadditions/events/EventEntry.java
+++ src/main/java/io/github/a1qs/vaultadditions/events/EventEntry.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.annotations.Expose;
-import io.github.a1qs.vaultadditions.config.ServerConfigs;
+import io.github.a1qs.vaultadditions.data.EventData;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -17,7 +17,16 @@ public class EventEntry {
     public final ResourceLocation eventId;
 
     @Expose
-    public final JsonElement eventMessage;
+    public final JsonElement eventStartMessage;
+
+    @Expose
+    public final JsonElement eventEndMessage;
+
+    @Expose
+    public final JsonElement eventLoginMessage;
+
+    @Expose
+    public final JsonElement eventDisplayMessage;
 
     @Expose
     public final long eventDuration;
@@ -34,20 +43,24 @@ public class EventEntry {
     @Expose
     public int maxCrystalsSubmitted;
 
-
-
-    public EventEntry(ResourceLocation eventId, JsonElement eventMessage, long eventDuration, boolean crystalSubmission) {
+    public EventEntry(ResourceLocation eventId, JsonElement eventStartMessage, JsonElement eventEndMessage, JsonElement eventLoginMessage, JsonElement eventDisplayMessage, long eventDuration, boolean crystalSubmission) {
         this.eventId = eventId;
-        this.eventMessage = eventMessage;
+        this.eventStartMessage = eventStartMessage;
+        this.eventEndMessage = eventEndMessage;
+        this.eventLoginMessage = eventLoginMessage;
+        this.eventDisplayMessage = eventDisplayMessage;
         this.eventDuration = eventDuration;
         this.crystalSubmission = crystalSubmission;
         if(crystalSubmission) minCrystalsSubmitted = 50;
         if(crystalSubmission) maxCrystalsSubmitted = 250;
     }
 
-    public EventEntry(ResourceLocation eventId, JsonElement eventMessage, long eventDuration, boolean crystalSubmission, List<ResourceLocation> resourceLocationList) {
+    public EventEntry(ResourceLocation eventId, JsonElement eventStartMessage, JsonElement eventEndMessage, JsonElement eventLoginMessage, JsonElement eventDisplayMessage, long eventDuration, boolean crystalSubmission, List<ResourceLocation> resourceLocationList) {
         this.eventId = eventId;
-        this.eventMessage = eventMessage;
+        this.eventStartMessage = eventStartMessage;
+        this.eventEndMessage = eventEndMessage;
+        this.eventLoginMessage = eventLoginMessage;
+        this.eventDisplayMessage = eventDisplayMessage;
         this.eventDuration = eventDuration;
         this.crystalSubmission = crystalSubmission;
         if(crystalSubmission) minCrystalsSubmitted = 50;
@@ -60,16 +73,33 @@ public class EventEntry {
      *
      * @return A parsed `Component` ready for rendering.
      */
-    public Component getParsedMessage() {
+    public Component getParsedMessage(JsonElement message) {
+        EventData d = EventData.getServer(); // bandaid fix LMAO
+        boolean eventActive = d.isEventActive();
         Map<String, String> placeholders = new HashMap<>();
-        placeholders.put("eventDuration", String.valueOf(this.eventDuration));
+
+        // static event data
+        placeholders.put("totalEventDurationTicks", String.valueOf(this.eventDuration));
+        placeholders.put("totalEventDurationSeconds", String.valueOf((this.eventDuration / 20) % 60));
+        placeholders.put("totalEventDurationMinutes", String.valueOf((this.eventDuration / (20 * 60)) % 60));
+        placeholders.put("totalEventDurationHours", String.valueOf((this.eventDuration / (20 * 60 * 60)) % 24));
+        placeholders.put("totalEventDurationDays", String.valueOf(this.eventDuration / (20 * 60 * 60 * 24)));
         placeholders.put("eventId", this.eventId.toString());
         placeholders.put("crystalSubmission", String.valueOf(this.crystalSubmission));
         placeholders.put("minCrystalsSubmitted", String.valueOf(this.minCrystalsSubmitted));
         placeholders.put("maxCrystalsSubmitted", String.valueOf(this.maxCrystalsSubmitted));
 
-        if (eventMessage.isJsonArray()) {
-            JsonArray messageArray = eventMessage.getAsJsonArray();
+        // active event based data
+        placeholders.put("eventDurationTicks", String.valueOf(eventActive ? d.getEventDuration() : null));
+        placeholders.put("eventDurationSeconds", String.valueOf(eventActive ? (d.getEventDuration() / 20) % 60 : null));
+        placeholders.put("eventDurationMinutes", String.valueOf(eventActive ? (d.getEventDuration() / 20 * 60) % 60 : null));
+        placeholders.put("eventDurationHours", String.valueOf(eventActive ? (d.getEventDuration() / (20 * 60 * 60)) % 24 : null));
+        placeholders.put("eventDurationDays", String.valueOf(eventActive ? d.getEventDuration() / (20 * 60 * 60 * 24) : null));
+        placeholders.put("requiredCrystals", String.valueOf(eventActive ? d.getActiveEvent().getRequiredCrystals() : null));
+        placeholders.put("submittedCrystals", String.valueOf(eventActive ? d.getActiveEvent().getCrystalsSubmitted() : null));
+
+        if (message.isJsonArray()) {
+            JsonArray messageArray = message.getAsJsonArray();
             JsonArray substitutedArray = new JsonArray();
 
             for (JsonElement element : messageArray) {
@@ -94,7 +124,7 @@ public class EventEntry {
      * @param placeholders A map of placeholder keys to their replacement values.
      * @return A JSON object with placeholders substituted.
      */
-    private JsonObject substitutePlaceholders(JsonObject json, Map<String, String> placeholders) {
+    private static JsonObject substitutePlaceholders(JsonObject json, Map<String, String> placeholders) {
         for (Map.Entry<String, String> entry : placeholders.entrySet()) {
             String placeholder = "{" + entry.getKey() + "}";
             if (json.has("text")) {
@@ -106,4 +136,48 @@ public class EventEntry {
     }
 
 
+
+    /**
+     *  Getter/Setters
+     */
+
+    public ResourceLocation getId() {
+        return eventId;
+    }
+
+    public JsonElement getEventStartMessage() {
+        return eventStartMessage;
+    }
+
+    public JsonElement getEventEndMessage() {
+        return eventEndMessage;
+    }
+
+    public JsonElement getEventLoginMessage() {
+        return eventLoginMessage;
+    }
+
+    public JsonElement getEventDisplayMessage() {
+        return eventDisplayMessage;
+    }
+
+    public long getEventDuration() {
+        return eventDuration;
+    }
+
+    public boolean isCrystalSubmission() {
+        return crystalSubmission;
+    }
+
+    public List<ResourceLocation> getAdditionalModifiers() {
+        return additionalModifiers;
+    }
+
+    public int getMinCrystalsSubmitted() {
+        return minCrystalsSubmitted;
+    }
+
+    public int getMaxCrystalsSubmitted() {
+        return maxCrystalsSubmitted;
+    }
 }
