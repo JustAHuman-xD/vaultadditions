aa728615f7dfcce213932bd468ca31116b94ede9
a1qs <ankiller2007.09.20@gmail.com>
2024-12-11 19:39:22 +0100

    functioning leaderboard menu

--- src/main/java/io/github/a1qs/vaultadditions/client/menu/LeaderboardMenu.java
+++ src/main/java/io/github/a1qs/vaultadditions/client/menu/LeaderboardMenu.java
@@ -32,27 +32,25 @@ public class LeaderboardMenu extends Screen {
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pPoseStack);
         drawCenteredString(pPoseStack, this.font, this.title, this.width / 2, 10, 0xFFFFFF);
-        renderLeaderboard(pPoseStack);
+        renderLeaderboard(pPoseStack, this.width / 2, 30);
 
         int renderX = (int) (this.width * 0.85);
         int renderY = (int) (this.height * 0.6);
         int scale = Math.min(this.width, this.height) / 8;
-
         float mouseXOffset = renderX - pMouseX;
-        float mouseYOffset = (float) (renderY - (pMouseY + scale)); // renderY - pMouseY
+        float mouseYOffset = renderY - (pMouseY + scale);
 
         InventoryScreen.renderEntityInInventory(renderX, renderY, scale, mouseXOffset, mouseYOffset, this.minecraft.player);
 
+        int positionY = renderY +10; // Add spacing below the player renderer
+        renderPlayerPosition(pPoseStack, renderX, positionY);
 
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
     }
 
 
 
-    private void renderLeaderboard(PoseStack pPoseStack) {
-        int x = this.width / 2;
-        int y = 30;
-
+    private void renderLeaderboard(PoseStack pPoseStack, int x, int startY) {
         // Create a sorted list of the leaderboard entries
         List<Map.Entry<UUID, Integer>> sortedLeaderboard = this.leaderboard.entrySet().stream()
                 .sorted((entry1, entry2) -> Integer.compare(entry2.getValue(), entry1.getValue()))
@@ -60,7 +58,11 @@ public class LeaderboardMenu extends Screen {
 
         List<Map.Entry<UUID, Integer>> top10 = sortedLeaderboard.subList(0, Math.min(10, sortedLeaderboard.size()));
 
-        int placement = 1; // Start with the 1st place
+        // Render the top 10 entries
+        int y = startY; // Start rendering leaderboard at the provided Y position
+        int lineSpacing = 15;
+        int placement = 1;
+
         for (Map.Entry<UUID, Integer> entry : top10) {
             UUID uuid = entry.getKey();
             int contributions = entry.getValue();
@@ -69,27 +71,28 @@ public class LeaderboardMenu extends Screen {
             MutableComponent textComponent = new TextComponent(String.format("#%d | ", placement))
                     .append(new TextComponent(username).withStyle(ChatFormatting.GREEN))
                     .append(new TextComponent(" | Contributions: ").withStyle(ChatFormatting.GRAY))
-                    .append(new TextComponent(String.valueOf(contributions)).withStyle(ChatFormatting.GOLD))
-                    .append(new TextComponent(" " + uuid).withStyle(ChatFormatting.GRAY));
+                    .append(new TextComponent(String.valueOf(contributions)).withStyle(ChatFormatting.GOLD));
 
+            // Centered leaderboard text
             Minecraft.getInstance().font.draw(pPoseStack, textComponent, x - this.font.width(textComponent) / 2.0f, y, 0xFFFFFF);
 
-            y += 15; // Move down for the next entry
+            y += lineSpacing;
             placement++;
         }
+    }
 
 
-        MutableComponent splitter = new TextComponent("--- Your Position ---").withStyle(ChatFormatting.YELLOW);
-        y += 15; // Add space before the splitter
-        Minecraft.getInstance().font.draw(pPoseStack, splitter, x - this.font.width(splitter) / 2.0f, y, 0xFFFFFF);
-
-        y += 15; // Move down after the splitter
-
-        // Find the player's placement and display it
+    private void renderPlayerPosition(PoseStack pPoseStack, int x, int y) {
         UUID playerUUID = Minecraft.getInstance().player.getUUID();
+        List<Map.Entry<UUID, Integer>> sortedLeaderboard = this.leaderboard.entrySet().stream()
+                .sorted((entry1, entry2) -> Integer.compare(entry2.getValue(), entry1.getValue()))
+                .toList();
+
         int playerPlacement = -1;
         int playerContributions = 0;
+        int lineSpacing = 15;
 
+        // Find player's position
         for (int i = 0; i < sortedLeaderboard.size(); i++) {
             if (sortedLeaderboard.get(i).getKey().equals(playerUUID)) {
                 playerPlacement = i + 1; // Placement is index + 1
@@ -104,11 +107,14 @@ public class LeaderboardMenu extends Screen {
                     .append(new TextComponent(" | Contributions: ").withStyle(ChatFormatting.GRAY))
                     .append(new TextComponent(String.valueOf(playerContributions)).withStyle(ChatFormatting.GOLD));
 
-            Minecraft.getInstance().font.draw(pPoseStack, playerComponent, x - this.font.width(playerComponent) / 2.0f, y, 0xFFFFFF);
+            Minecraft.getInstance().font.draw(pPoseStack, playerComponent, x - this.font.width(playerComponent) / 2.0f, y + lineSpacing, 0xFFFFFF);
         } else {
             MutableComponent notFound = new TextComponent("Your position could not be found!").withStyle(ChatFormatting.RED);
-            Minecraft.getInstance().font.draw(pPoseStack, notFound, x - this.font.width(notFound) / 2.0f, y, 0xFFFFFF);
+            Minecraft.getInstance().font.draw(pPoseStack, notFound, x - this.font.width(notFound) / 2.0f, y + lineSpacing, 0xFFFFFF);
         }
+
+        MutableComponent splitter = new TextComponent("--- Your Position ---").withStyle(ChatFormatting.YELLOW);
+        Minecraft.getInstance().font.draw(pPoseStack, splitter, x - this.font.width(splitter) / 2.0f, y, 0xFFFFFF);
     }
 
 
