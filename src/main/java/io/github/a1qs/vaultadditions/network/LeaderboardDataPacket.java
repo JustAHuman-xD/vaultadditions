9a15e786e4cb13a88e160465301a7d56094b4e29
a1qs <ankiller2007.09.20@gmail.com>
2024-12-11 18:30:09 +0100

    Create LeaderboardDataPacket for requesting data LeaderboardRequestPacket for fetching data

---
+++ src/main/java/io/github/a1qs/vaultadditions/network/LeaderboardDataPacket.java
@@ -0,0 +1,50 @@
+package io.github.a1qs.vaultadditions.network;
+
+import io.github.a1qs.vaultadditions.client.menu.LeaderboardMenu;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public class LeaderboardDataPacket {
+    private final Map<UUID, Integer> leaderboard;
+
+    public LeaderboardDataPacket(Map<UUID, Integer> leaderboard) {
+        this.leaderboard = leaderboard;
+    }
+
+    public static void encode(LeaderboardDataPacket msg, FriendlyByteBuf buffer) {
+        buffer.writeInt(msg.leaderboard.size());
+        msg.leaderboard.forEach((uuid, count) -> {
+            buffer.writeUUID(uuid);
+            buffer.writeInt(count);
+        });
+    }
+
+    public static LeaderboardDataPacket decode(FriendlyByteBuf buffer) {
+        int size = buffer.readInt();
+        Map<UUID, Integer> leaderboard = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            UUID uuid = buffer.readUUID();
+            int score = buffer.readInt();
+            leaderboard.put(uuid, score);
+        }
+        return new LeaderboardDataPacket(leaderboard);
+    }
+
+    public static void handle(LeaderboardDataPacket msg, Supplier<NetworkEvent.Context> contextSupplier) {
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().setScreen(new LeaderboardMenu(new TranslatableComponent("dev.leaderboard.test"), msg.leaderboard));
+        });
+        contextSupplier.get().setPacketHandled(true);
+    }
+
+    public Map<UUID, Integer> getLeaderboard() {
+        return leaderboard;
+    }
+}
---
+++ src/main/java/io/github/a1qs/vaultadditions/network/LeaderboardRequestPacket.java
@@ -0,0 +1,29 @@
+package io.github.a1qs.vaultadditions.network;
+
+import io.github.a1qs.vaultadditions.data.PowerCrystalData;
+import io.github.a1qs.vaultadditions.init.ModNetwork;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public class LeaderboardRequestPacket {
+    public LeaderboardRequestPacket() {}
+
+    public static void encode(LeaderboardRequestPacket msg, FriendlyByteBuf buffer) {}
+
+    public static LeaderboardRequestPacket decode(FriendlyByteBuf buffer) {
+        return new LeaderboardRequestPacket();
+    }
+
+    public static void handle(LeaderboardRequestPacket msg, Supplier<NetworkEvent.Context> contextSupplier) {
+        ServerPlayer player = contextSupplier.get().getSender();
+        if (player != null) {
+            PowerCrystalData data = PowerCrystalData.getServer();
+            ModNetwork.sendToClient(new LeaderboardDataPacket(data.getPlayerContributionsMap()), player);
+        }
+        contextSupplier.get().setPacketHandled(true);
+    }
+}
