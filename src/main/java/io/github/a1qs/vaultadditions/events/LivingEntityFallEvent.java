c449def15c8682adcdcef78b0fd01990d0be1db8
a1qs <ankiller2007.09.20@gmail.com>
2024-12-14 21:37:43 +0100

    add docs to MixinMultiJumpTrinket & LivingEntityFallEvent

--- src/main/java/io/github/a1qs/vaultadditions/events/LivingEntityFallEvent.java
+++ src/main/java/io/github/a1qs/vaultadditions/events/LivingEntityFallEvent.java
@@ -10,10 +10,18 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class LivingEntityFallEvent {
 
+    /**
+     * Handles the reduction/cancellation of fall damage for players with the Feather Trinket equipped.
+     * <p>
+     * If the player has an active and usable {@code MultiJumpTrinket}:
+     * - Fall damage is canceled if the fall distance is less than 5.0 blocks to prevent damage ticking when taking no damage.
+     * - Otherwise, the fall distance is reduced by 2.0 blocks to mitigate fall damage.
+     * <p>
+     * Called whenever a LivingEntity falls
+     */
     @SubscribeEvent
     public static void multiJumpTrinketFallReductionEvent(LivingFallEvent event) {
         if(event.getEntityLiving() instanceof ServerPlayer player) {
-            // -2
             if (!TrinketHelper.getTrinkets(player, MultiJumpTrinket.class).stream().noneMatch((trinket) -> trinket.isUsable(player))) {
                 if(event.getDistance() < 5.0F) {
                     event.setCanceled(true);
--- src/main/java/io/github/a1qs/vaultadditions/mixins/MixinMultiJumpTrinket.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinMultiJumpTrinket.java
@@ -12,11 +12,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinMultiJumpTrinket {
     @Shadow private static boolean clientIsJumpHeld = false;
 
+    /**
+     * Fixes an issue present when holding the jump button and landing on the ground again,
+     * <p>
+     * If the player held the jump button as they land, they would not receive the additional velocity boost upwards,
+     * this mixin aims to resolve this.
+     */
     @Inject(
             method = "onClientTick",
             at = @At(value = "FIELD",
                     target = "Liskallia/vault/gear/trinket/effects/MultiJumpTrinket;clientJumpCount:I",
-            ordinal = 0),
+                    ordinal = 0),
             require = 1
     )
     private void afterResetJumpCount(Player player, CallbackInfo ci) {
