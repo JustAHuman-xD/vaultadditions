5b9b46888e7b497fb1da93c8bf41957bc68e271e
a1qs <ankiller2007.09.20@gmail.com>
2024-12-14 20:42:45 +0100

    refactor data/request packet to include active event

--- src/main/java/io/github/a1qs/vaultadditions/network/LeaderboardDataPacket.java
+++ src/main/java/io/github/a1qs/vaultadditions/network/LeaderboardDataPacket.java
@@ -2,6 +2,7 @@ package io.github.a1qs.vaultadditions.network;
 
 import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.client.menu.LeaderboardMenu;
+import io.github.a1qs.vaultadditions.events.VaultAdditionsEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -15,10 +16,12 @@ import java.util.function.Supplier;
 public class LeaderboardDataPacket {
     private final Map<UUID, Integer> leaderboard;
     private final String nextScheduledEvent;
+    private final VaultAdditionsEvent optionalEvent;
 
-    public LeaderboardDataPacket(Map<UUID, Integer> leaderboard, String nextScheduledEvent) {
+    public LeaderboardDataPacket(Map<UUID, Integer> leaderboard, String nextScheduledEvent, VaultAdditionsEvent optionalEvent) {
         this.leaderboard = leaderboard;
         this.nextScheduledEvent = nextScheduledEvent;
+        this.optionalEvent = optionalEvent;
     }
 
     public static void encode(LeaderboardDataPacket msg, FriendlyByteBuf buffer) {
@@ -29,6 +32,15 @@ public class LeaderboardDataPacket {
         });
 
         buffer.writeUtf(msg.nextScheduledEvent);
+
+        if (msg.optionalEvent != null) {
+            buffer.writeBoolean(true); // Flag to indicate presence
+            buffer.writeInt(msg.optionalEvent.getConfigIndex());
+            buffer.writeInt(msg.optionalEvent.getRequiredCrystals());
+            buffer.writeInt(msg.optionalEvent.getCrystalsSubmitted());
+        } else {
+            buffer.writeBoolean(false); // No event present
+        }
     }
 
     public static LeaderboardDataPacket decode(FriendlyByteBuf buffer) {
@@ -41,7 +53,16 @@ public class LeaderboardDataPacket {
         }
 
         String nextEvent = buffer.readUtf();
-        return new LeaderboardDataPacket(leaderboard, nextEvent);
+
+        VaultAdditionsEvent optionalEvent = null;
+        if (buffer.readBoolean()) { // Check if event is present
+            int configIndex = buffer.readInt();
+            int requiredCrystals = buffer.readInt();
+            int crystalsSubmitted = buffer.readInt();
+            optionalEvent = new VaultAdditionsEvent(configIndex, requiredCrystals, crystalsSubmitted);
+        }
+
+        return new LeaderboardDataPacket(leaderboard, nextEvent, optionalEvent);
     }
 
     public static void handle(LeaderboardDataPacket msg, Supplier<NetworkEvent.Context> contextSupplier) {
@@ -50,7 +71,8 @@ public class LeaderboardDataPacket {
                     new LeaderboardMenu(
                             new TranslatableComponent("screen." + VaultAdditions.MOD_ID + ".leaderboard"),
                             msg.leaderboard,
-                            msg.nextScheduledEvent
+                            msg.nextScheduledEvent,
+                            msg.optionalEvent
                     )
             );
         });
