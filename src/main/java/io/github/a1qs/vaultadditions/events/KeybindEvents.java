134c96f8f610cc59033eb55c1770ebb85d376327
a1qs <ankiller2007.09.20@gmail.com>
2024-12-21 14:15:20 +0100

    message refactor

--- src/main/java/io/github/a1qs/vaultadditions/events/KeybindEvents.java
+++ src/main/java/io/github/a1qs/vaultadditions/events/KeybindEvents.java
@@ -3,12 +3,10 @@ package io.github.a1qs.vaultadditions.events;
 import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.init.ModKeybinds;
 import io.github.a1qs.vaultadditions.init.ModNetwork;
-import io.github.a1qs.vaultadditions.network.LeaderboardRequestPacket;
+import io.github.a1qs.vaultadditions.network.LeaderboardRequestMessage;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,7 +22,7 @@ public class KeybindEvents {
 
         if(ModKeybinds.openLeaderboard.consumeClick()) {
             if(Minecraft.getInstance().player != null) {
-                ModNetwork.sendToServer(new LeaderboardRequestPacket());
+                ModNetwork.sendToServer(new LeaderboardRequestMessage());
             }
         }
 
