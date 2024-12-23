0bd3dcee50a538ee63f2ace91e8b430327f83119
a1qs <ankiller2007.09.20@gmail.com>
2024-12-23 01:11:55 +0100

    add ModModels class to handle custom Models

---
+++ src/main/java/io/github/a1qs/vaultadditions/init/ModModels.java
@@ -0,0 +1,36 @@
+package io.github.a1qs.vaultadditions.init;
+
+import io.github.a1qs.vaultadditions.vault.gear.model.armor.layers.HoyArmorLayers;
+import io.github.a1qs.vaultadditions.vault.gear.model.armor.layers.HoyGroguArmorLayers;
+import iskallia.vault.VaultMod;
+import iskallia.vault.dynamodel.DynamicModelProperties;
+import iskallia.vault.dynamodel.model.armor.ArmorModel;
+import iskallia.vault.dynamodel.model.item.HandHeldModel;
+import net.minecraft.world.entity.EquipmentSlot;
+
+public class ModModels {
+    public static DynamicModelProperties STANDARD_PROPERTIES = new DynamicModelProperties().allowTransmogrification().discoverOnRoll();
+
+    public static class Armor {
+        public static final ArmorModel HOY_82 = new ArmorModel(VaultMod.id("gear/armor/hoy"), "Beskar")
+                .properties(STANDARD_PROPERTIES)
+                .usingLayers(new HoyArmorLayers())
+                .addSlot(EquipmentSlot.HEAD)
+                .addSlot(EquipmentSlot.CHEST)
+                .addSlot(EquipmentSlot.LEGS)
+                .addSlot(EquipmentSlot.FEET);
+
+        public static final ArmorModel HOY_82_GROGU = new ArmorModel(VaultMod.id("gear/armor/hoy_with_grogu"), "Beskar & Grogu")
+                .properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll())
+                .usingLayers(new HoyGroguArmorLayers())
+                .addSlot(EquipmentSlot.HEAD)
+                .addSlot(EquipmentSlot.CHEST)
+                .addSlot(EquipmentSlot.LEGS)
+                .addSlot(EquipmentSlot.FEET);
+    }
+
+    public static class WoldsBattleStaffs {
+        public static final HandHeldModel DARKSABER =
+                new HandHeldModel(VaultMod.id("gear/battlestaff/darksaber"), "The Darksaber").properties(STANDARD_PROPERTIES);
+    }
+}
--- src/main/java/io/github/a1qs/vaultadditions/mixins/MixinModDynamicModels$Armor.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinModDynamicModels$Armor.java
@@ -1,33 +1,22 @@
 package io.github.a1qs.vaultadditions.mixins;
 
-import io.github.a1qs.vaultadditions.vault.gear.model.armor.layers.HoyArmorLayers;
-import iskallia.vault.VaultMod;
-import iskallia.vault.dynamodel.DynamicModelProperties;
-import iskallia.vault.dynamodel.model.armor.ArmorModel;
+import io.github.a1qs.vaultadditions.init.ModModels;
 import iskallia.vault.dynamodel.registry.ArmorPieceModelRegistry;
-
 import iskallia.vault.init.ModDynamicModels;
-import net.minecraft.world.entity.EquipmentSlot;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ModDynamicModels.Armor.class, remap = false)
 public class MixinModDynamicModels$Armor {
     @Shadow @Final public static ArmorPieceModelRegistry PIECE_REGISTRY;
 
-    private static final ArmorModel HOY_82 =
-            PIECE_REGISTRY.registerAll(
-            new ArmorModel(VaultMod.id("gear/armor/hoy"), "HoY_82")
-                    .properties(new DynamicModelProperties()
-                            .allowTransmogrification()
-                            .discoverOnRoll()
-                    )
-                    .usingLayers(new HoyArmorLayers())
-                    .addSlot(EquipmentSlot.HEAD)
-                    .addSlot(EquipmentSlot.CHEST)
-                    .addSlot(EquipmentSlot.LEGS)
-                    .addSlot(EquipmentSlot.FEET)
-    );
-
+    @Inject(method = "<clinit>", at = @At(value = "TAIL"))
+    private static void injectArmorModels(CallbackInfo ci) {
+        PIECE_REGISTRY.registerAll(ModModels.Armor.HOY_82);
+        PIECE_REGISTRY.registerAll(ModModels.Armor.HOY_82_GROGU);
+    }
 }
