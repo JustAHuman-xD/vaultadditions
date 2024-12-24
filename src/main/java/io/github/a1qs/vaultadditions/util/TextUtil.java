2178229ec9be05ce54eed668f6be2551de4fc950
a1qs <ankiller2007.09.20@gmail.com>
2024-12-24 16:14:25 +0100

    update TextUtil with constant list of rainbow hex codes

--- src/main/java/io/github/a1qs/vaultadditions/util/TextUtil.java
+++ src/main/java/io/github/a1qs/vaultadditions/util/TextUtil.java
@@ -7,8 +7,22 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.network.chat.TextComponent;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TextUtil {
-    public static Component blendString(String text, float interval, String... hexStrings) {
+    public static final List<String> RAINBOW = Arrays.asList(
+            "#FF0000",
+            "#FF7F00",
+            "#FFFF00",
+            "#00FF00",
+            "#0000FF",
+            "#4B0082",
+            "#8B00FF"
+    );
+
+
+    public static Component blendString(String text, float interval, List<String> hexStrings) {
         ColorBlender colorBlender = new ColorBlender(1.0F);
 
         for(String hexString : hexStrings) {
