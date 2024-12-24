79f9dfd916edd4da5768e83cb0ef20f362ce517c
a1qs <ankiller2007.09.20@gmail.com>
2024-12-24 14:26:33 +0100

    Register PowerCrystalExtension ClockModifier

---
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinVaultRegistry.java
@@ -0,0 +1,24 @@
+package io.github.a1qs.vaultadditions.mixins;
+
+import io.github.a1qs.vaultadditions.vault.core.time.modifier.PowerCrystalExtension;
+import iskallia.vault.core.data.key.registry.SupplierRegistry;
+import iskallia.vault.core.vault.VaultRegistry;
+import iskallia.vault.core.vault.time.modifier.ClockModifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = VaultRegistry.class, remap = false)
+public class MixinVaultRegistry {
+    @Shadow
+    @Final
+    public static SupplierRegistry<ClockModifier> CLOCK_MODIFIER;
+
+    @Inject(method = "<clinit>", at = @At(value = "TAIL"))
+    private static void injectRegistries(CallbackInfo ci) {
+        CLOCK_MODIFIER.add(PowerCrystalExtension.KEY);
+    }
+}
---
+++ src/main/java/io/github/a1qs/vaultadditions/vault/core/time/modifier/PowerCrystalExtension.java
@@ -0,0 +1,44 @@
+package io.github.a1qs.vaultadditions.vault.core.time.modifier;
+
+import iskallia.vault.core.Version;
+import iskallia.vault.core.data.adapter.Adapters;
+import iskallia.vault.core.data.key.FieldKey;
+import iskallia.vault.core.data.key.SupplierKey;
+import iskallia.vault.core.data.key.registry.FieldRegistry;
+import iskallia.vault.core.vault.time.TickClock;
+import iskallia.vault.core.vault.time.modifier.ClockModifier;
+import net.minecraft.server.level.ServerLevel;
+
+import java.util.UUID;
+
+public class PowerCrystalExtension extends ClockModifier {
+    public static final SupplierKey<ClockModifier> KEY = SupplierKey.of("power_crystal", ClockModifier.class).with(Version.v1_0, PowerCrystalExtension::new);
+    public static final FieldRegistry FIELDS = ClockModifier.FIELDS.merge(new FieldRegistry());
+    public static final FieldKey<UUID> VAULT_ID = FieldKey.of("vault_id", UUID.class).with(Version.v1_0, Adapters.UUID, DISK.all()).register(FIELDS);
+    public static final FieldKey<Integer> INCREMENT = FieldKey.of("increment", Integer.class).with(Version.v1_0, Adapters.INT_SEGMENTED_7, DISK.all()).register(FIELDS);
+
+    public PowerCrystalExtension(UUID vaultId, int increment) {
+        this.set(VAULT_ID, vaultId);
+        this.set(INCREMENT, increment);
+    }
+
+    protected PowerCrystalExtension() {
+    }
+
+    @Override
+    public SupplierKey<ClockModifier> getKey() {
+        return KEY;
+    }
+
+    @Override
+    public FieldRegistry getFields() {
+        return FIELDS;
+    }
+
+    @Override
+    protected void apply(ServerLevel serverLevel, TickClock tickClock) {
+        tickClock.set(TickClock.DISPLAY_TIME, tickClock.get(TickClock.DISPLAY_TIME) + this.get(INCREMENT));
+        this.set(CONSUMED);
+    }
+
+}
