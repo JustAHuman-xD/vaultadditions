db89736ce5b1cedb9f44dbe42146e4a5d8db296a
a1qs <ankiller2007.09.20@gmail.com>
2024-12-28 19:43:37 +0100

    i truly love gradle (removed conditionalmixin, added woldsvaults dependency)

--- src/main/java/io/github/a1qs/vaultadditions/mixins/MixinBattlestaffs.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinBattlestaffs.java
@@ -3,10 +3,7 @@ package io.github.a1qs.vaultadditions.mixins;
 
 import io.github.a1qs.vaultadditions.init.ModModels;
 import iskallia.vault.dynamodel.model.item.HandHeldModel;
-import iskallia.vault.dynamodel.registry.ArmorPieceModelRegistry;
 import iskallia.vault.dynamodel.registry.DynamicModelRegistry;
-import me.fallenbreath.conditionalmixin.api.annotation.Condition;
-import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.iwolfking.woldsvaults.models.Battlestaffs;
 
-@Restriction(require = @Condition("woldsvaults"))
+
 @Mixin(value = Battlestaffs.class, remap = false)
 public class MixinBattlestaffs {
     @Shadow
