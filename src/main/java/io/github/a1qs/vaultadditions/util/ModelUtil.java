89d0a0dbaf652edd2f831a20465e7e1b0d8bdcd7
a1qs <ankiller2007.09.20@gmail.com>
2025-01-13 18:13:31 +0100

    Added ModelUtil to check for Vault Armor models

---
+++ src/main/java/io/github/a1qs/vaultadditions/util/ModelUtil.java
@@ -0,0 +1,69 @@
+package io.github.a1qs.vaultadditions.util;
+
+import io.github.a1qs.vaultadditions.init.ModModels;
+import iskallia.vault.dynamodel.model.armor.ArmorModel;
+import iskallia.vault.dynamodel.model.armor.ArmorPieceModel;
+import iskallia.vault.gear.data.VaultGearData;
+import iskallia.vault.init.ModGearAttributes;
+import iskallia.vault.item.gear.VaultArmorItem;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+
+public class ModelUtil {
+
+    public static boolean isWearingHoySet(Player player) {
+        return isWearingArmorSet(ModModels.Armor.HOY_82, player) || isWearingArmorSet(ModModels.Armor.HOY_82_GROGU, player);
+    }
+
+    public static boolean isWearingHokageRobesSet(Player player) {
+        return isWearingArmorSet(ModModels.Armor.HOKAGE_ROBES, player) || isWearingArmorSet(ModModels.Armor.HOKAGE_ROBES_MASKLESS, player);
+    }
+
+
+
+    public static boolean isWearingArmorPiece(ArmorModel model, EquipmentSlot slot, Player player) {
+        if(model.getPiece(slot).isPresent()) {
+            ArmorPieceModel pieceModel = model.getPiece(slot).get();
+
+            ItemStack equipmentStack = player.getInventory().armor.get(equipmentSlotToInventoryIndex(slot));
+
+            if (!(equipmentStack.getItem() instanceof VaultArmorItem)) return false;
+            VaultGearData gearData = VaultGearData.read(equipmentStack);
+            ResourceLocation modelId = gearData.getFirstValue(ModGearAttributes.GEAR_MODEL).orElse(null);
+
+            if (modelId == null) return false;
+
+            return pieceModel.getId().equals(modelId);
+        }
+        return false;
+    }
+
+    public static boolean isWearingArmorSet(ArmorModel armorSet, Player player) {
+         return armorSet.getPieces().entrySet().stream().allMatch(entry -> {
+            EquipmentSlot equipmentSlot = entry.getKey();
+            ArmorPieceModel pieceModel = entry.getValue();
+            ItemStack equipmentStack = player.getInventory().armor.get(equipmentSlotToInventoryIndex(equipmentSlot));
+
+            if (!(equipmentStack.getItem() instanceof VaultArmorItem)) return false;
+
+            VaultGearData gearData = VaultGearData.read(equipmentStack);
+            ResourceLocation modelId = gearData.getFirstValue(ModGearAttributes.GEAR_MODEL).orElse(null);
+            if (modelId == null) return false;
+
+            return pieceModel.getId().equals(modelId);
+        });
+    }
+
+    private static int equipmentSlotToInventoryIndex(EquipmentSlot equipmentSlot) {
+        return switch (equipmentSlot) {
+            case HEAD -> 3;
+            case CHEST -> 2;
+            case LEGS -> 1;
+            case FEET -> 0;
+            default -> -1;
+        };
+    }
+}
