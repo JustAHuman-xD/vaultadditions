e5bab70bb5248f3bbac18335e80902ccad687a08
a1qs <ankiller2007.09.20@gmail.com>
2025-03-15 22:24:24 +0100

    feat:add more helper attribute methods

--- src/main/java/io/github/a1qs/vaultadditions/util/VaultGearAttributeHelper.java
+++ src/main/java/io/github/a1qs/vaultadditions/util/VaultGearAttributeHelper.java
@@ -1,7 +1,7 @@
 package io.github.a1qs.vaultadditions.util;
 
 import iskallia.vault.gear.attribute.VaultGearAttributeInstance;
-import iskallia.vault.gear.attribute.ability.AbilityLevelAttribute;
+import iskallia.vault.gear.attribute.ability.*;
 import iskallia.vault.gear.attribute.custom.effect.EffectGearAttribute;
 import iskallia.vault.init.ModGearAttributes;
 import net.minecraft.world.effect.MobEffect;
@@ -9,6 +9,22 @@ import net.minecraft.world.effect.MobEffect;
 public class VaultGearAttributeHelper {
     public static VaultGearAttributeInstance<?> abilityLevelAttributeInstance(String name, int levelChange) {
         return VaultGearAttributeInstance.cast(ModGearAttributes.ABILITY_LEVEL, new AbilityLevelAttribute(name, levelChange));
+    }
+
+    public static VaultGearAttributeInstance<?> abilityCooldownFlatAttributeInstance(String name, float amount) {
+        return VaultGearAttributeInstance.cast(ModGearAttributes.ABILITY_COOLDOWN_FLAT, new AbilityCooldownFlatAttribute(name, amount));
+    }
+
+    public static VaultGearAttributeInstance<?> abilityCooldownPercentageAttributeInstance(String abilityName, float amount) {
+        return VaultGearAttributeInstance.cast(ModGearAttributes.ABILITY_COOLDOWN_PERCENT, new AbilityCooldownPercentAttribute(abilityName, amount));
+    }
+
+    public static VaultGearAttributeInstance<?> abilityManaCostFlatAttributeInstance(String name, float amount) {
+        return VaultGearAttributeInstance.cast(ModGearAttributes.ABILITY_MANACOST_FLAT, new AbilityManaCostPercentAttribute(name, amount));
+    }
+
+    public static VaultGearAttributeInstance<?> abilityManaCostPercentageAttributeInstance(String abilityName, float amount) {
+        return VaultGearAttributeInstance.cast(ModGearAttributes.ABILITY_MANACOST_PERCENT, new AbilityManaCostPercentAttribute(abilityName, amount));
     }
 
     public static VaultGearAttributeInstance<?> potionEffectVaultAttributeInstance(MobEffect effect, int amplifier) {
