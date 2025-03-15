a7217ec9bd464171ce59f12b9dfd78398d6cfb72
a1qs <ankiller2007.09.20@gmail.com>
2025-03-15 20:49:28 +0100

    chore: register SIZE_SCALE with a vanilla association

---
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinModGearAttributes.java
@@ -0,0 +1,28 @@
+package io.github.a1qs.vaultadditions.mixins;
+
+import com.google.common.collect.Table;
+import iskallia.vault.gear.attribute.VaultGearAttribute;
+import iskallia.vault.init.ModAttributes;
+import iskallia.vault.init.ModGearAttributes;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = ModGearAttributes.class, remap = false)
+public class MixinModGearAttributes {
+
+    @Shadow
+    @Final
+    private static Table<Attribute, AttributeModifier.Operation, VaultGearAttribute<?>> VANILLA_ATTRIBUTES;
+
+    @Inject(method = "registerAssociations", at = @At("TAIL"))
+    private static void registerAdditionalAssociations(CallbackInfo ci) {
+        VANILLA_ATTRIBUTES.put(ModAttributes.SIZE_SCALE, AttributeModifier.Operation.MULTIPLY_TOTAL, io.github.a1qs.vaultadditions.init.vault.ModGearAttributes.SIZE_SCALE);
+    }
+
+}
