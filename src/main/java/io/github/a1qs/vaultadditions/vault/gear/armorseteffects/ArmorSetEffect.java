f8b3468f346332284c1e8b9fe22f15a5d0668e74
a1qs <ankiller2007.09.20@gmail.com>
2025-03-15 20:50:11 +0100

    chore: change getTooltipComponent to public access level

--- src/main/java/io/github/a1qs/vaultadditions/vault/gear/armorseteffects/ArmorSetEffect.java
+++ src/main/java/io/github/a1qs/vaultadditions/vault/gear/armorseteffects/ArmorSetEffect.java
@@ -1,11 +1,9 @@
 package io.github.a1qs.vaultadditions.vault.gear.armorseteffects;
 
 import iskallia.vault.gear.attribute.VaultGearAttributeInstance;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.server.level.ServerPlayer;
 
 public abstract class ArmorSetEffect {
     public abstract VaultGearAttributeInstance<?> getVaultGearAttributeInstance();
-    protected abstract MutableComponent getTooltipComponent();
+    public abstract MutableComponent getTooltipComponent();
 }
