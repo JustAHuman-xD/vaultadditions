41763362c406250b1757598d26ff7b810ed5a67e
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-14 20:35:52 -0500

    simplify that mixin

--- src/main/java/io/github/a1qs/vaultadditions/mixins/armor_effects/MixinElytraLayer.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/armor_effects/MixinElytraLayer.java
@@ -1,14 +1,11 @@
 package io.github.a1qs.vaultadditions.mixins.armor_effects;
 
 import io.github.a1qs.vaultadditions.config.Configs;
-import io.github.a1qs.vaultadditions.util.ModelUtil;
 import io.github.a1qs.vaultadditions.vault.gear.effect.HideElytraTransmogEffect;
-import iskallia.vault.item.gear.VaultArmorItem;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.ElytraLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -26,11 +23,8 @@ public abstract class MixinElytraLayer<T extends LivingEntity, M extends EntityM
 
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true, remap = false)
     public void shouldRender(ItemStack stack, T entity, CallbackInfoReturnable<Boolean> cir) {
-        if (entity instanceof Player player) {
-            ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
-            if (Configs.TRANSMOG_EFFECTS_CONFIG.hasEffect(player, HideElytraTransmogEffect.INSTANCE)) {
-                cir.setReturnValue(false);
-            }
+        if (entity instanceof Player player && Configs.TRANSMOG_EFFECTS_CONFIG.hasEffect(player, HideElytraTransmogEffect.INSTANCE)) {
+            cir.setReturnValue(false);
         }
     }
 }
