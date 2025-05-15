2a43453d0249e37d6c7728e2b8ebcd74118c7211
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-14 20:31:13 -0500

    also hide elytra if its a set effect

--- src/main/java/io/github/a1qs/vaultadditions/mixins/armor_effects/MixinElytraLayer.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/armor_effects/MixinElytraLayer.java
@@ -1,6 +1,7 @@
 package io.github.a1qs.vaultadditions.mixins.armor_effects;
 
 import io.github.a1qs.vaultadditions.config.Configs;
+import io.github.a1qs.vaultadditions.util.ModelUtil;
 import io.github.a1qs.vaultadditions.vault.gear.effect.HideElytraTransmogEffect;
 import iskallia.vault.item.gear.VaultArmorItem;
 import net.minecraft.client.model.EntityModel;
@@ -26,8 +27,9 @@ public abstract class MixinElytraLayer<T extends LivingEntity, M extends EntityM
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true, remap = false)
     public void shouldRender(ItemStack stack, T entity, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof Player player) {
-            ItemStack chest = player.getInventory().getArmor(EquipmentSlot.CHEST.getIndex());
-            if (chest.getItem() instanceof VaultArmorItem && Configs.TRANSMOG_EFFECTS_CONFIG.hasEffect(chest, HideElytraTransmogEffect.INSTANCE)) {
+            ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
+            if (Configs.TRANSMOG_EFFECTS_CONFIG.hasEffect(chest, HideElytraTransmogEffect.INSTANCE)
+                || Configs.TRANSMOG_EFFECTS_CONFIG.hasEffect(ModelUtil.getWornSet(player), HideElytraTransmogEffect.INSTANCE)) {
                 cir.setReturnValue(false);
             }
         }
