2ace58c7d60dd2a9a8845f4fb524c13b38606f3b
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-20 12:44:53 -0500

    Whoops

--- src/main/java/io/github/a1qs/vaultadditions/mixins/rotateable_fools_gold/MixinModBlocks.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/rotateable_fools_gold/MixinModBlocks.java
@@ -1,17 +1,18 @@
 package io.github.a1qs.vaultadditions.mixins.rotateable_fools_gold;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.a1qs.vaultadditions.block.RotateableFoolsGold;
 import iskallia.vault.init.ModBlocks;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ModBlocks.class, remap = false)
 public class MixinModBlocks {
-    @WrapOperation(method = "<clinit>", at = @At("TAIL"))
-    private static void rotateableFoolsGold() {
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void rotateableFoolsGold(CallbackInfo ci) {
         AccessorModBlocks.setFOOLS_GOLD_BLOCK(new RotateableFoolsGold(BlockBehaviour.Properties.copy(Blocks.GOLD_BLOCK)));
     }
 }
