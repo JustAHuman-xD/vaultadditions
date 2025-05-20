557c0e794fdc04adba95276ff4b6a27a2602de4e
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-20 00:23:51 -0500

    also transparent items

--- src/main/java/io/github/a1qs/vaultadditions/vault/gear/gecko/item/VaultGeckoItemRenderer.java
+++ src/main/java/io/github/a1qs/vaultadditions/vault/gear/gecko/item/VaultGeckoItemRenderer.java
@@ -1,16 +1,19 @@
 package io.github.a1qs.vaultadditions.vault.gear.gecko.item;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import io.github.a1qs.vaultadditions.util.ModelUtil;
 import io.github.a1qs.vaultadditions.vault.gear.gecko.VaultGeckoModel;
 import io.github.a1qs.vaultadditions.vault.gear.gecko.VaultGeckoModelProvider;
 import iskallia.vault.gear.item.VaultGearItem;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.IItemRenderProperties;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 
@@ -40,5 +43,10 @@ public class VaultGeckoItemRenderer<T extends Item & VaultGearItem & IAnimatable
     @Override
     public ResourceLocation getTextureLocation(T animatable) {
         return ((VaultGeckoModelProvider<?>) getGeoModelProvider()).getTextureLocation(this.currentItemStack);
+    }
+
+    @Override
+    public RenderType getRenderType(T animatable, float partialTick, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight, ResourceLocation texture) {
+        return RenderType.entityTranslucent(texture);
     }
 }
