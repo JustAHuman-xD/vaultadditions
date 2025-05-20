c12e245aebeb9672f133f8b7fa04d39dee44966d
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-20 15:05:43 -0500

    new attempt

--- src/main/java/io/github/a1qs/vaultadditions/mixins/rotateable_fools_gold/MixinModBlocks.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/rotateable_fools_gold/MixinModBlocks.java
@@ -1,30 +1,21 @@
 package io.github.a1qs.vaultadditions.mixins.rotateable_fools_gold;
 
 import io.github.a1qs.vaultadditions.block.RotateableFoolsGold;
-import iskallia.vault.VaultMod;
 import iskallia.vault.init.ModBlocks;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.Registry;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.event.RegistryEvent;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = ModBlocks.class, remap = false)
-public abstract class MixinModBlocks {
-    @Inject(method = "registerBlocks", at = @At("TAIL"))
-    private static void rotateableFoolsGold(RegistryEvent.Register<Block> event, CallbackInfo ci) {
-        Block foolsGold = new RotateableFoolsGold(BlockBehaviour.Properties.copy(Blocks.GOLD_BLOCK));
-        AccessorModBlocks.setFOOLS_GOLD_BLOCK(foolsGold);
-        registerBlock(event, foolsGold, VaultMod.id("fools_gold"));
-    }
-
-    @Shadow
-    private static void registerBlock(RegistryEvent.Register<Block> event, Block block, ResourceLocation id) {
-        throw new AssertionError("Mixin failed to apply");
+public class MixinModBlocks {
+    @Redirect(method = "<clinit>", at = @At(value = "FIELD", target = "Liskallia/vault/init/ModBlocks;FOOLS_GOLD_BLOCK:Lnet/minecraft/world/level/block/Block;", opcode = Opcodes.PUTSTATIC))
+    private static void rotateableFoolsGold(Block value) {
+        ((AccessorMappedRegistry<Block>) Registry.BLOCK).getIntrusiveHolderCache().remove(value);
+        AccessorModBlocks.setFOOLS_GOLD_BLOCK(new RotateableFoolsGold(BlockBehaviour.Properties.copy(Blocks.GOLD_BLOCK)));
     }
 }
