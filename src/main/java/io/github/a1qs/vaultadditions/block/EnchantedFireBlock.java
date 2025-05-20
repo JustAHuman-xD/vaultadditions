a7c4297788d91d8803302037842337f2e33d0dfa
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-20 13:01:14 -0500

    don't burn

--- src/main/java/io/github/a1qs/vaultadditions/block/EnchantedFireBlock.java
+++ src/main/java/io/github/a1qs/vaultadditions/block/EnchantedFireBlock.java
@@ -1,12 +1,22 @@
 package io.github.a1qs.vaultadditions.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
 public class EnchantedFireBlock extends BaseFireBlock {
     public EnchantedFireBlock(Properties pProperties) {
         super(pProperties, 0);
+    }
+
+    @Override @ParametersAreNonnullByDefault
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        // Do nothing
     }
 
     @Override
