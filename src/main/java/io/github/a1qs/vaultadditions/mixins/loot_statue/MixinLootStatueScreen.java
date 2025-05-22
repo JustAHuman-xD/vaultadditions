d230ac651ef0c5142380378bd03214201cf0d16c
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-21 21:32:01 -0500

    Don't pick omega statue item

---
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/loot_statue/MixinLootStatueScreen.java
@@ -0,0 +1,15 @@
+package io.github.a1qs.vaultadditions.mixins.loot_statue;
+
+import iskallia.vault.client.gui.screen.LootStatueScreen;
+import net.minecraftforge.network.simple.SimpleChannel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(LootStatueScreen.class)
+public class MixinLootStatueScreen {
+    @Redirect(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/network/simple/SimpleChannel;sendToServer(Ljava/lang/Object;)V"))
+    public <MSG> void preventSelectingOnClose(SimpleChannel instance, MSG message) {
+        // Just don't send this bruh
+    }
+}
