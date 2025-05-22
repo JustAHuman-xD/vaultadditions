7b1431e88e90386be8d82f2400f42975e0435886
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-22 01:26:21 -0500

    null safety

--- src/main/java/io/github/a1qs/vaultadditions/vault/menu/PowerPanRegion.java
+++ src/main/java/io/github/a1qs/vaultadditions/vault/menu/PowerPanRegion.java
@@ -1,9 +1,11 @@
 package io.github.a1qs.vaultadditions.vault.menu;
 
+import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.config.Configs;
 import iskallia.vault.client.gui.screen.player.legacy.tab.split.pan.SkillPanRegion;
 import iskallia.vault.client.gui.screen.player.legacy.widget.TalentGroupWidget;
 import iskallia.vault.config.entry.SkillStyle;
+import iskallia.vault.skill.base.Skill;
 import iskallia.vault.skill.base.TieredSkill;
 import net.minecraft.network.chat.TextComponent;
 
@@ -17,13 +19,17 @@ public class PowerPanRegion extends SkillPanRegion<PowerTree, PowerElementContai
         super(parentScreen, new TextComponent("Talents Tab"), powerDialog);
     }
 
+    @Override
     protected void initSkillWidget(PowerTree skillTree, String skillName, SkillStyle style, Map<String, PowerWidget> widgets, List<TalentGroupWidget> groups) {
-        widgets.put(skillName, new PowerWidget((TieredSkill)skillTree.getForId(skillName).orElse(null), skillTree, style));
+        Skill skill = skillTree.getForId(skillName).orElse(null);
+        if (skill instanceof TieredSkill tieredSkill) {
+            widgets.put(skillName, new PowerWidget(tieredSkill, skillTree, style));
+        } else {
+            VaultAdditions.LOGGER.error("Power skill {} is not a TieredSkill", skillName);
+        }
     }
 
     protected HashMap<String, SkillStyle> getStyles() {
         return Configs.POWERS_GUI.getStyles();
     }
-
-
 }
